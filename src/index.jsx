f10a3ff29543d326abd99f7681da43049c48d54e
Sanphet Somjit <xsplaystation5@gmail.com>
2025-01-12 21:33:41 +0700

    Add Project

--- src/index.jsx
+++ src/index.jsx
@@ -3,11 +3,13 @@ import './styles.css'
 import App from './App'
 import Underlay from './Underlayer';
 import AboutMe from './AboutMe';
+import Projects from './Projects';
 
 createRoot(document.getElementById('root')).render(
   <>
     <App />
     <Underlay />
     <AboutMe />
+    <Projects />
   </>
 )
